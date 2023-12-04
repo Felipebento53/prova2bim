20180c0d409a890ec52aa5f820ee9fd3c9bcaf74
Felipebento53 <felipebento53@outlook.com>
2023-12-04 11:37:07 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Menu from "./templates/Menu";
 import TelaChat from "./templates/TelaChat";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import store from "./Redux/store";
 import UsuarioForm from "./Cadastros/UsuarioForm";
 import { Provider } from "react-redux";//componente
@@ -11,14 +11,14 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
-            <Route path="/usuarios" element={<UsuarioForm/>} />
-            <Route path="/" element={<Menu/>} />
-            <Route path="/chat" element={<TelaChat/>}/>
-            <Route path="*"/>
-          </Routes>
-        </BrowserRouter>
+              <Route path="/usuarios" element={<UsuarioForm/>} />
+              <Route path="/" element={<Menu/>} />
+              <Route path="/chat" element={<TelaChat/>}/>
+              <Route path="*"/>
+            </Routes>
+        </HashRouter>
       </Provider>
     </div>
   );
