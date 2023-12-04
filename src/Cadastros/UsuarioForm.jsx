b3675f062cb423b80beaf2ff7578924af2e65f00
Felipebento53 <felipebento53@outlook.com>
2023-12-04 10:00:53 -0300

    cadUsuarios final

--- src/Cadastros/UsuarioForm.jsx
+++ src/Cadastros/UsuarioForm.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux"
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Menu from "../templates/Menu";
 
 
 
@@ -15,29 +16,31 @@ export default function UsuarioForm(props){
 
     const dispatch = useDispatch();
 
-
     return(
-    <Form>
-    <br />
-    <br /> 
-    <h1>Cadastro de Usuarios</h1>
-    <hr />
-      <Form.Group className="mb-3" controlId="nick">
-        <Form.Label>Nickname</Form.Label>
-        <Form.Control type="text" placeholder="Nome de usuario" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="url">
-        <Form.Label>Avatar</Form.Label>
-        <Form.Control type="text" placeholder="url do avatar" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="data">
-        <Form.Label>Data Ingresso</Form.Label>
-        <Form.Control type="date" placeholder="Data do ingresso" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Cadastrar
-      </Button>
-    </Form>
+    <div>
+        <Menu/>
+        <br />
+        <br /> 
+        <h1>Cadastro de Usuarios</h1>
+        <hr />
+        <Form>
+        <Form.Group className="mb-3" controlId="nick">
+            <Form.Label>Nickname</Form.Label>
+            <Form.Control type="text" placeholder="Nome de usuario" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="url">
+            <Form.Label>Avatar</Form.Label>
+            <Form.Control type="text" placeholder="url do avatar" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="data">
+            <Form.Label>Data Ingresso</Form.Label>
+            <Form.Control type="date" placeholder="Data do ingresso" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Cadastrar
+        </Button>
+        </Form>
+    </div>
   );
     
 }
